c3f278b17a3b51ff379100e26754db930d2b837f
Rick Klaasboer <rick@klaasboer.org>
2022-08-16 00:36:33 +0200

    feat(command): create kortebroek command

---
+++ src/commands/KortebroekCommand.ts
@@ -0,0 +1,43 @@
+import {ChatInputCommandInteraction, CacheType, EmbedBuilder} from 'discord.js';
+import Command from './Command';
+import fetch from 'node-fetch';
+import {JSDOM} from 'jsdom';
+
+export const KortebroekCommand = new Command<
+    ChatInputCommandInteraction<CacheType>
+>({
+    name: 'kanikeenkortebroekaan',
+    description: 'Kan ik een korte broek aan?',
+    run: async (interaction) => {
+        // Get response body from kanikeenkortebroekaan.nl
+        const request = await fetch('https://www.kanikeenkortebroekaan.nl/');
+        const response = await request.text();
+
+        // Create a fake DOM from this response
+        const dom = new JSDOM(response);
+        const document = dom.window.document;
+        const {src} = document.getElementsByClassName('main-image')[0]
+            .children[0] as HTMLImageElement;
+
+        // Get date, image and weather forcast from this DOM
+        const date = (
+            document.getElementsByClassName('date')[0] as HTMLDivElement
+        )?.innerHTML;
+        const tempContainer = document.getElementsByClassName(
+            'temp',
+        )[0] as HTMLDivElement;
+        const temp = (tempContainer.children[0] as HTMLParagraphElement)
+            .innerHTML;
+        const rain = (tempContainer.children[1] as HTMLParagraphElement)
+            .innerHTML;
+
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setImage('https://www.kanikeenkortebroekaan.nl' + src)
+                    .setTitle('Kan ik een korte broek aan?')
+                    .setDescription(`${date}\n- ${temp}\n- ${rain}`.trim()),
+            ],
+        });
+    },
+});
