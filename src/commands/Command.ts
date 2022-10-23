9e780a5c746befb0de9252745a20598bd8bfca2d
Rick Klaasboer <rick@klaasboer.org>
2022-10-23 15:03:19 +0200

    feat: extend base command to support options

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -1,17 +1,56 @@
+import {Maybe} from '@/types/util';
+
+export enum APPLICATION_COMMAND_OPTIONS {
+    SUB_COMMAND = 1,
+    SUB_COMMAND_GROUP = 2,
+    STRING = 3,
+    INTEGER = 4,
+    BOOLEAN = 5,
+    USER = 6,
+    CHANNEL = 7,
+    ROLE = 8,
+    MENTIONABLE = 9,
+    NUMBER = 10,
+    ATTACHMENT = 11,
+}
+
+type ApplicationCommandOption = {
+    name: string;
+    value: string | number;
+};
+
+type SlashCommandOptions = {
+    type: APPLICATION_COMMAND_OPTIONS;
+    name: string;
+    description: string;
+    required?: boolean;
+    choices?: ApplicationCommandOption[];
+    options?: SlashCommandOptions[];
+    channel_types?: [];
+    min_value?: number;
+    max_value?: number;
+    min_length?: number;
+    max_length?: number;
+    autocomplete?: boolean;
+}[];
+
 type Props<T> = {
     name: string;
     description: string;
+    options?: SlashCommandOptions;
     run(interaction: T): void | Promise<void>;
 };
 
 export default class Command<T> {
     public name: string;
     public description: string;
+    public options: Maybe<SlashCommandOptions>;
     public run: (interaction: T) => void | Promise<void>;
 
-    constructor({name, description, run}: Props<T>) {
+    constructor({name, description, options, run}: Props<T>) {
         this.name = name;
         this.description = description;
+        this.options = options;
         this.run = run;
     }
 }
