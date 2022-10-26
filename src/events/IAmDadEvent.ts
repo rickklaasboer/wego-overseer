5cf4cbb51c00462afc60182d2fb957abefc4a556
StefanEilander2 <s1157160@student.windesheim.nl>
2022-10-26 22:41:44 +0200

    Removed capital sensitivity

--- src/events/IAmDadEvent.ts
+++ src/events/IAmDadEvent.ts
@@ -9,19 +9,16 @@ export const IAmDadEvent = new Event<'messageCreate'>({
         const {content} = message;
 
         // Check if string includes 'ik ben' or 'i am'
-        if(content !== 'ik ben' && content !== 'i am' ){
-            if (content.toLowerCase().includes('ik ben')) {
-            
-                const part = content.split('ik ben')[1].trim();
-                const msg = `Hoi ${part}, ik ben Wego Overseer!`;
+        if (content.toLowerCase().includes('ik ben')) {
+            const part = content.toLowerCase().split('ik ben')[1].trim();
+            const msg = `Hoi ${part}, ik ben Wego Overseer!`;
 
-                message.reply(msg);
-            } else if (content.toLowerCase().includes('i am')) {
-                const part = content.split('i am')[1].trim();
-                const msg = `Hi ${part}, I am Wego Overseer!`;
+            message.reply(msg);
+        } else if (content.toLowerCase().includes('i am')) {
+            const part = content.toLowerCase().split('i am')[1].trim();
+            const msg = `Hi ${part}, I am Wego Overseer!`;
 
-                message.reply(msg);
-            }
-    }
+            message.reply(msg);
+        }
     },
 });
