5e8041ce57459b226bb02645543cc3c102b208a3
Rick Klaasboer <rick@klaasboer.org>
2022-11-25 20:35:47 +0100

    fix: wrap events in try..catch blocks to prevent hard bot crash

--- src/events/IAmDadEvent.ts
+++ src/events/IAmDadEvent.ts
@@ -1,24 +1,31 @@
+import Logger from '@/telemetry/logger';
 import Event from './Event';
+
+const logger = new Logger('wego-overseer:BangerEvent');
 
 export const IAmDadEvent = new Event<'messageCreate'>({
     name: 'messageCreate',
     run: async (message) => {
-        // Terminate if user is a bot
-        if (message.author.bot) return;
+        try {
+            // Terminate if user is a bot
+            if (message.author.bot) return;
 
-        const {content} = message;
+            const {content} = message;
 
-        // Check if string includes 'ik ben' or 'i am'
-        if (content.toLowerCase().includes('ik ben')) {
-            const part = content.toLowerCase().split('ik ben')[1].trim();
-            const msg = `Hoi ${part}, ik ben Wego Overseer!`;
+            // Check if string includes 'ik ben' or 'i am'
+            if (content.toLowerCase().includes('ik ben')) {
+                const part = content.toLowerCase().split('ik ben')[1].trim();
+                const msg = `Hoi ${part}, ik ben Wego Overseer!`;
 
-            message.reply(msg);
-        } else if (content.toLowerCase().includes('i am')) {
-            const part = content.toLowerCase().split('i am')[1].trim();
-            const msg = `Hi ${part}, I am Wego Overseer!`;
+                message.reply(msg);
+            } else if (content.toLowerCase().includes('i am')) {
+                const part = content.toLowerCase().split('i am')[1].trim();
+                const msg = `Hi ${part}, I am Wego Overseer!`;
 
-            message.reply(msg);
+                message.reply(msg);
+            }
+        } catch (err) {
+            logger.fatal('Unable to handle IAmDadEvent', err);
         }
     },
 });
