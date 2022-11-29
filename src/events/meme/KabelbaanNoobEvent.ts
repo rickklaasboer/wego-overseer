ba8fe8cae25859da266dc9f3970d93296a3b90ba
Niek Kleverkamp <74187513+NiekKleverkamp@users.noreply.github.com>
2022-11-29 20:02:41 +0100

    Moved variables outside of event

--- src/events/meme/KabelbaanNoobEvent.ts
+++ src/events/meme/KabelbaanNoobEvent.ts
@@ -2,17 +2,18 @@ import Logger from '@/telemetry/logger';
 import Event from '../Event';
 
 const logger = new Logger('wego-overseer:KabelBaanNoobEvent');
+const kabelbaanDiscordId = "162624307745390592";
+const noobEmojiId = "375338339248898048"
 
 export const KabelbaanNoobEvent = new Event<'messageCreate'>({
     name: 'messageCreate',
     run: async (message) => {
         try {
-            const kabelbaanDiscordId = "162624307745390592";
             // Terminate if user is not kabelbaan
             if(message.author.id != kabelbaanDiscordId) return;
 
             //<:noob:375338339248898048> | <:name:id>
-            const noobEmoji = message.client.emojis.cache.find(emoji => emoji.id == "375338339248898048");
+            const noobEmoji = message.client.emojis.cache.find(emoji => emoji.id == noobEmojiId);
 
             if(!noobEmoji){
                 logger.error("Noob emoji not found")
