79f4365aadff36c88850a62d96635d6fa8bbe92f
Rick Klaasboer <rick@klaasboer.org>
2022-12-11 18:05:26 +0100

    feat: handle karma events with basic predicates and functionality

--- src/events/karma/KarmaRemoveDownvoteEvent.ts
+++ src/events/karma/KarmaRemoveDownvoteEvent.ts
@@ -1,12 +1,18 @@
+import {ensureChannelIsAvailable} from '@/commands/karma/KarmaCommand/predicates';
 import Logger from '@/telemetry/logger';
 import Event from '../Event';
 
 const logger = new Logger('wego-overseer:KarmaRemoveDownvoteEvent');
 
-export const KarmaRemoveDownvoteEvent = new Event<'messageCreate'>({
-    name: 'messageCreate',
-    enabled: false,
-    run: async (message) => {
+export const KarmaRemoveDownvoteEvent = new Event<'messageReactionRemove'>({
+    name: 'messageReactionRemove',
+    run: async (reaction) => {
+        const channel = await ensureChannelIsAvailable(
+            reaction.message.channel.id,
+            reaction.message.guild?.id,
+        );
+
+        if (!channel.isKarmaChannel) return;
         logger.debug('KarmaRemoveDownvoteEvent received');
     },
 });
--- src/events/karma/KarmaUpvoteEvent.ts
+++ src/events/karma/KarmaUpvoteEvent.ts
@@ -1,12 +1,18 @@
+import {ensureChannelIsAvailable} from '@/commands/karma/KarmaCommand/predicates';
 import Logger from '@/telemetry/logger';
 import Event from '../Event';
 
 const logger = new Logger('wego-overseer:KarmaUpvoteEvent');
 
-export const KarmaUpvoteEvent = new Event<'messageCreate'>({
-    name: 'messageCreate',
-    enabled: false,
-    run: async (message) => {
+export const KarmaUpvoteEvent = new Event<'messageReactionAdd'>({
+    name: 'messageReactionAdd',
+    run: async (reaction) => {
+        const channel = await ensureChannelIsAvailable(
+            reaction.message.channel.id,
+            reaction.message.guild?.id,
+        );
+
+        if (!channel.isKarmaChannel) return;
         logger.debug('KarmaUpvoteEvent received');
     },
 });
