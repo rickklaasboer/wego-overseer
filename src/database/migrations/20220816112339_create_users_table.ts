ab256759bfa42145e2b4bc64dd13dfaf34857079
Rick Klaasboer <rick@klaasboer.org>
2022-12-11 17:25:18 +0100

    fix: remove unused xp columns from user migration/entity

--- src/database/migrations/20220816112339_create_users_table.ts
+++ src/database/migrations/20220816112339_create_users_table.ts
@@ -3,8 +3,6 @@ import {Knex} from 'knex';
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('users', (table) => {
         table.string('id').primary();
-        table.bigInteger('xp').unsigned();
-        table.dateTime('xpAwardedAt');
         table.timestamps({useCamelCase: true});
     });
 }
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -2,8 +2,6 @@ import Model from '@/entities/Model';
 
 export default class User extends Model {
     id!: string;
-    xp!: number;
-    xpAwardedAt!: string;
 
     static get tableName() {
         return 'users';
