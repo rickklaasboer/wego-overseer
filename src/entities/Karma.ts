c31ddd7f0f57f58b4fea2bd0cd808117979a54a1
Rick Klaasboer <rick@klaasboer.org>
2022-12-11 19:55:02 +0100

    fix: add relationship mappings to entities

--- src/entities/Karma.ts
+++ src/entities/Karma.ts
@@ -1,4 +1,8 @@
 import Model from '@/entities/Model';
+import {RelationMappings} from 'objection';
+import Channel from './Channel';
+import Guild from './Guild';
+import User from './User';
 
 export default class Karma extends Model {
     id!: string;
@@ -8,6 +12,43 @@ export default class Karma extends Model {
     receivedFromUserId!: string;
     channelId!: string;
     messageId!: string;
+
+    static get relationMappings(): RelationMappings {
+        return {
+            guild: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Guild,
+                join: {
+                    from: 'karma.guildId',
+                    to: 'guilds.id',
+                },
+            },
+            user: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: 'karma.userId',
+                    to: 'users.id',
+                },
+            },
+            receivedFrom: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: 'karma.receivedFromUserId',
+                    to: 'users.id',
+                },
+            },
+            channel: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Channel,
+                join: {
+                    from: 'karma.channelId',
+                    to: 'channels.id',
+                },
+            },
+        };
+    }
 
     static get tableName() {
         return 'karma';
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,7 +1,30 @@
 import Model from '@/entities/Model';
+import {RelationMappings} from 'objection';
+import Karma from './Karma';
 
 export default class User extends Model {
     id!: string;
+
+    static get relationMappings(): RelationMappings {
+        return {
+            receivedKarma: {
+                relation: Model.HasManyRelation,
+                modelClass: Karma,
+                join: {
+                    from: 'karma.userId',
+                    to: 'users.id',
+                },
+            },
+            handedKarma: {
+                relation: Model.HasManyRelation,
+                modelClass: Karma,
+                join: {
+                    from: 'karma.receivedFromUserId',
+                    to: 'users.id',
+                },
+            },
+        };
+    }
 
     static get tableName() {
         return 'users';
