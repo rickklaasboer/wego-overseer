28cfd194f243e602ecae100dc107b73fcdfa3535
Rick Klaasboer <rick@klaasboer.org>
2022-12-11 20:07:49 +0100

    feat: add command to get karma of self or user

--- src/commands/karma/KarmaUserGetCommand.ts
+++ src/commands/karma/KarmaUserGetCommand.ts
@@ -1,9 +1,22 @@
 import Command from '@/commands/Command';
+import Karma from '@/entities/Karma';
 
 export const KarmaUserGetCommand = new Command({
     name: 'internal',
     description: 'internal',
     run: async (interaction) => {
-        //
+        const user = interaction.options.getUser('user') ?? interaction.user;
+
+        const sum = (await Karma.query()
+            .where({
+                guildId: interaction.guild?.id,
+                userId: user.id,
+            })
+            .sum('amount as totalKarma')
+            .first()) as Karma & {totalKarma: number};
+
+        await interaction.reply(
+            `${user.username} has a total of ${sum.totalKarma} karma`,
+        );
     },
 });
