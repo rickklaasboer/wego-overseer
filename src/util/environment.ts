837ceff4dbe5f6a43918cf0571bcbdb1e8482903
Rick Klaasboer <rick@klaasboer.org>
2023-05-16 19:16:20 +0200

    fix: add env variable to enable/disable knex logging

--- src/util/environment.ts
+++ src/util/environment.ts
@@ -12,3 +12,19 @@ export function getEnvInt(key: string, fallback?: number): Maybe<number> {
     const value = parseInt(String(process.env[key]));
     return Number.isFinite(value) ? value : fallback;
 }
+
+export function getEnvBool(key: string): Maybe<boolean>;
+export function getEnvBool(key: string, fallback: boolean): boolean;
+export function getEnvBool(key: string, fallback?: boolean): Maybe<boolean> {
+    const value = String(process.env[key]).trim();
+
+    if (/^(?:y|yes|true|1|on)$/i.test(value)) {
+        return true;
+    }
+
+    if (/^(?:n|no|false|0|off)$/i.test(value)) {
+        return false;
+    }
+
+    return fallback;
+}
