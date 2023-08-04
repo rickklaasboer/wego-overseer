314570154c7689f9c869d741051cca60b7837b7e
Rick Klaasboer <rick@klaasboer.org>
2023-08-04 15:47:09 +0200

    feat: set experience command

--- src/commands/experience/ExperienceSetCommand.ts
+++ src/commands/experience/ExperienceSetCommand.ts
@@ -1,7 +1,25 @@
+import ExperienceService from '@/services/ExperienceService';
+import {isAdmin} from '@/util/discord';
+import {trans} from '@/util/localization';
 import InternalCommand from '../InternalCommand';
 
 export const ExperienceSetCommand = new InternalCommand({
     run: async (interaction) => {
-        // TODO: Implement
+        if (!isAdmin(interaction)) {
+            await interaction.followUp(
+                trans('commands.experience.common.not_admin'),
+            );
+            return;
+        }
+
+        const guildId = interaction.guild?.id ?? '';
+        const user = interaction.options.getUser('user') ?? interaction.user;
+        const amount = interaction.options.getInteger('amount') ?? 0;
+
+        await ExperienceService.setExperience(guildId, user.id, amount);
+
+        await interaction.followUp(
+            trans('commands.experience.set.success', user.username, amount),
+        );
     },
 });
