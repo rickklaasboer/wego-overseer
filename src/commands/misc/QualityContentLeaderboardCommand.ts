45db4c6f3ab88c71fd9f63b5a10b5ece20c47a7d
Rick Klaasboer <rick@klaasboer.org>
2023-12-24 21:00:49 +0100

    fix: quality content leaderboard argument order

--- src/commands/misc/QualityContentLeaderboardCommand.ts
+++ src/commands/misc/QualityContentLeaderboardCommand.ts
@@ -2,6 +2,7 @@ import Command from '@/commands/Command';
 import Logger from '@/telemetry/logger';
 import StringBuilder from '@/util/StringBuilder';
 import {safeFetchUser} from '@/util/discord';
+import {getEnvInt} from '@/util/environment';
 import {t} from '@/util/localization';
 import {EmbedBuilder} from 'discord.js';
 
@@ -23,6 +24,8 @@ type Row = {
     totalKarma: number;
 };
 
+const QCC_MIN_EMOJI_COUNT = getEnvInt('QCC_MIN_EMOJI_COUNT', 5);
+
 export const QualityContentLeaderboardCommand = new Command({
     name: 'ccleaderboard',
     description: 'Display the quality content leaderboard',
@@ -33,9 +36,10 @@ export const QualityContentLeaderboardCommand = new Command({
                 .select(
                     db.raw('*, count(*) as upvotes, SUM(amount) as totalKarma'),
                 )
+                .where('guildId', interaction.guildId)
                 .groupBy('messageId')
                 .having('totalKarma', '>', 0)
-                .having('upvotes', '>=', 5)
+                .having('upvotes', '>=', QCC_MIN_EMOJI_COUNT)
                 .orderBy('upvotes', 'desc')
                 .limit(10)) as Row[];
 
@@ -55,9 +59,9 @@ export const QualityContentLeaderboardCommand = new Command({
                         'commands.quality_content.embed.row',
                         user.username,
                         String(result.upvotes),
-                        String(result.totalKarma),
-                        result.messageId,
+                        String(result.messageId),
                         result.guildId,
+                        result.messageId,
                         result.channelId,
                     ),
                 );
