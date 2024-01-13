35d760b5b442f7b3fd97eed0c55f5d5fef95b00e
Rick Klaasboer <rick@klaasboer.org>
2024-01-13 22:10:05 +0100

    fix(command): update WhereMemeCommand to use tsyringe

--- src/commands/meme/WhereMemeCommand.ts
+++ src/commands/meme/WhereMemeCommand.ts
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import Command, {APPLICATION_COMMAND_OPTIONS} from '@/commands/Command';
+import {APPLICATION_COMMAND_OPTIONS} from '@/commands/Command';
 import Jimp from 'jimp';
 import {Base64JimpImage} from '@/util/Base64JimpImage';
 import {trans} from '@/util/localization';
-import Logger from '@/telemetry/logger';
-
-const logger = new Logger('wego-overseer:commands:WhereMemeCommand');
+import BaseCommand, {DefaultInteraction} from '@/commands/BaseCommand';
+import {injectable} from 'tsyringe';
 
 // Magic constants
 const IMAGE_OFFSETS = {
@@ -13,11 +11,11 @@ const IMAGE_OFFSETS = {
     y: 24,
 };
 
-export const WhereMemeCommand = new Command({
-    name: 'where',
-    description: 'where x meme generator',
-    shouldDeferReply: true,
-    options: [
+@injectable()
+export default class WhereMemeCommand implements BaseCommand {
+    name = 'where';
+    description = 'Where monke meme generator';
+    options = [
         {
             type: APPLICATION_COMMAND_OPTIONS.STRING,
             name: 'text',
@@ -26,9 +24,12 @@ export const WhereMemeCommand = new Command({
             min_length: 1,
             max_length: 32,
         },
-    ],
-    run: async (interaction) => {
+    ];
+
+    public async execute(interaction: DefaultInteraction): Promise<void> {
         try {
+            await interaction.deferReply();
+
             const font = await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
             const img = await Jimp.read('./src/img/meme/monke.png');
 
@@ -53,11 +54,10 @@ export const WhereMemeCommand = new Command({
             const wrappedImage = new Base64JimpImage(img);
             await interaction.followUp({files: [wrappedImage.toAttachment()]});
         } catch (err) {
-            logger.fatal(err);
             await interaction.followUp({
                 content: trans('errors.common.failed', 'where meme'),
                 ephemeral: true,
             });
         }
-    },
-});
+    }
+}
