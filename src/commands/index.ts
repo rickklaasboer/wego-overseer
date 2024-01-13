8fe4a4c1f0bf9d4eddc950b5cf55cd8c61604d4d
Rick Klaasboer <rick@klaasboer.org>
2024-01-13 22:11:41 +0100

    fix: remove HelpCommand

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,10 @@
 import {AdventOfCodeCommand} from '@/commands/misc/AdventOfCodeCommand';
-import {HelpCommand} from '@/commands/misc/HelpCommand';
 import {KarmaCommand} from '@/commands/karma/KarmaCommand';
 import {PollCommand} from '@/commands/poll';
 import {QualityContentLeaderboardCommand} from './misc/QualityContentLeaderboardCommand';
 
 export default [
     AdventOfCodeCommand,
-    HelpCommand,
     KarmaCommand,
     PollCommand,
     QualityContentLeaderboardCommand,
