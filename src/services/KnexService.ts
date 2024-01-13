25d2d5f63d720d4c2ab9b161f7ffc3c53316e493
Rick Klaasboer <rick@klaasboer.org>
2024-01-13 16:33:03 +0100

    fix(services): bootstrap KnexService

---
+++ src/services/KnexService.ts
@@ -0,0 +1,33 @@
+import {getEnvString} from '@/util/environment';
+import knex, {Knex} from 'knex';
+import {singleton} from 'tsyringe';
+
+const DB_CLIENT = getEnvString('DB_CLIENT', '');
+const DB_HOST = getEnvString('DB_HOST', '');
+const DB_USER = getEnvString('DB_USER', '');
+const DB_PASSWORD = getEnvString('DB_PASSWORD', '');
+const DB_DATABASE = getEnvString('DB_DATABASE', '');
+
+@singleton()
+export default class KnexService {
+    private knex: Knex;
+
+    constructor() {
+        this.knex = knex({
+            client: DB_CLIENT,
+            connection: {
+                host: DB_HOST,
+                user: DB_USER,
+                password: DB_PASSWORD,
+                database: DB_DATABASE,
+            },
+        });
+    }
+
+    /**
+     * Get the Knex instance
+     */
+    public getKnex(): Knex {
+        return this.knex;
+    }
+}
