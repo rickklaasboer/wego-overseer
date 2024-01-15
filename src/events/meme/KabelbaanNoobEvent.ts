e1c0432f1330633f263a3a0d23d7a24a59c1818e
Rick Klaasboer <rick@klaasboer.org>
2024-01-15 15:58:39 +0100

    fix(event): update KabelbaanNoobEvent to use tsyringe

--- src/events/meme/KabelbaanNoobEvent.ts
+++ src/events/meme/KabelbaanNoobEvent.ts
@@ -1,33 +1,42 @@
-import Logger from '@/telemetry/logger';
-import Event from '../Event';
+import config from '@/config';
+import BaseEvent from '@/events/BaseEvent';
+import {GuildEmoji, Message} from 'discord.js';
+import {injectable} from 'tsyringe';
 
-const logger = new Logger('wego-overseer:events:KabelBaanNoobEvent');
+@injectable()
+export default class KabelbaanNoobEvent implements BaseEvent<'messageCreate'> {
+    public name = 'KabelbaanNoobEvent';
+    public event = 'messageCreate' as const;
 
-const kabelbaanDiscordId = '162624307745390592';
-const noobEmojiId = '375338339248898048';
-
-export const KabelbaanNoobEvent = new Event({
-    name: 'messageCreate',
-    run: async (_, message) => {
+    /**
+     * Run the event
+     */
+    public async execute(message: Message<boolean>): Promise<void> {
         try {
             // Terminate if user is not kabelbaan
-            if (message.author.id != kabelbaanDiscordId) return;
+            if (message.author.id != config.noob.kabelkaanDiscordId) return;
 
-            //<:noob:375338339248898048> | <:name:id>
-            const noobEmoji = message.client.emojis.cache.find(
-                (emoji) => emoji.id == noobEmojiId,
-            );
+            const emoji = this.getEmoji(message, config.noob.noobEmojiId);
 
-            if (!noobEmoji) {
-                logger.error('Noob emoji not found');
-                return;
-            }
-
-            if (message.content.includes(noobEmoji.toString())) {
-                await message.react(noobEmoji.toString());
-            }
+            if (!message.content.includes(emoji.toString())) return;
+            await message.react(emoji.toString());
         } catch (err) {
-            logger.error('Unable to handle KabelBaanNoobEvent', err);
+            console.error('Unable to handle KabelBaanNoobEvent', err);
         }
-    },
-});
+    }
+
+    /**
+     * Get emoji by id
+     */
+    private getEmoji(message: Message, emojiId: string): GuildEmoji {
+        const emoji = message.client.emojis.cache.find(
+            (emoji) => emoji.id == emojiId,
+        );
+
+        if (!emoji) {
+            throw new Error(`Emoji with id ${emojiId} not found`);
+        }
+
+        return emoji;
+    }
+}
