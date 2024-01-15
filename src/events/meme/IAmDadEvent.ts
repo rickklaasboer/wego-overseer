a2a3791de57fc7dfbdf9c3ffec8bcdbdb1cf03c2
Rick Klaasboer <rick@klaasboer.org>
2024-01-15 15:59:57 +0100

    fix(event): update IAmDadEvent' to use tsyringe

--- src/events/meme/IAmDadEvent.ts
+++ src/events/meme/IAmDadEvent.ts
@@ -1,61 +1,79 @@
-import Logger from '@/telemetry/logger';
 import {randomNumber} from '@/util/karma';
 import {trans} from '@/util/localization';
-import Event from '../Event';
+import BaseEvent from '@/events/BaseEvent';
+import {Message} from 'discord.js';
+import {injectable} from 'tsyringe';
 
-const logger = new Logger('wego-overseer:events:IAmDadEvent');
+@injectable()
+export default class IAmDadEvent implements BaseEvent<'messageCreate'> {
+    public name = 'IAmDadEvent';
+    public event = 'messageCreate' as const;
+    private possibleParts = {
+        nl: 'ik ben',
+        en: 'i am',
+    };
 
-const DUTCH_PART = 'ik ben';
-const ENGLISH_PART = 'i am';
-
-/**
- * Get part of message after 'i am' or 'ik ben'
- */
-function getPart(text: string, isDutch: boolean, isEnglish: boolean): string {
-    const separator = isDutch ? DUTCH_PART : isEnglish ? ENGLISH_PART : '';
-    return text.split(separator)[1].trim();
-}
-
-/**
- * Check if text includes either 'ik ben' or 'i am'
- */
-function isValid(text: string): [boolean, boolean] {
-    return [text.includes(DUTCH_PART), text.includes(ENGLISH_PART)];
-}
-
-/**
- * Determine locale, prefers Dutch
- */
-function getLocale(isDutch: boolean, isEnglish: boolean): string {
-    return isDutch ? 'nl' : isEnglish ? 'en' : '';
-}
-
-export const IAmDadEvent = new Event({
-    name: 'messageCreate',
-    run: async (_, message) => {
+    /**
+     * Run the event
+     */
+    public async execute(message: Message<boolean>): Promise<void> {
         try {
             // Terminate if user is a bot
             if (message.author.bot) return;
 
             const lower = message.content.toLowerCase();
-            const [isDutch, isEnglish] = isValid(lower);
+            const [isDutch, isEnglish] = this.isValid(lower);
 
             if (!isDutch && !isEnglish) return;
 
-            const part = getPart(lower, isDutch, isEnglish);
-            const locale = getLocale(isDutch, isEnglish);
+            const part = this.getPart(lower, isDutch, isEnglish);
+            const locale = this.getLocale(isDutch, isEnglish);
 
             // Super 100% random chance if the event should fire or not
             // this is to prevent spam
             const shouldFire = randomNumber(1, 10) === 7;
 
-            if (shouldFire) {
-                await message.reply(
-                    trans({phrase: 'events.iamdadevent.msg', locale}, part),
-                );
-            }
+            if (!shouldFire) return;
+
+            await message.reply(
+                trans({phrase: 'events.iamdadevent.msg', locale}, part),
+            );
         } catch (err) {
-            logger.fatal('Unable to handle IAmDadEvent', err);
+            console.error('Unable to handle IAmDadEvent', err);
         }
-    },
-});
+    }
+
+    /**
+     * Get part of message after 'i am' or 'ik ben'
+     */
+    private getPart(
+        text: string,
+        isDutch: boolean,
+        isEnglish: boolean,
+    ): string {
+        const separator = isDutch
+            ? this.possibleParts.nl
+            : isEnglish
+            ? this.possibleParts.en
+            : '';
+
+        return text.split(separator)[1].trim();
+    }
+
+    /**
+     * Check if text includes either 'ik ben' or 'i am'
+     */
+    private isValid(text: string): [boolean, boolean] {
+        return [
+            text.includes(this.possibleParts.nl),
+            text.includes(this.possibleParts.en),
+        ];
+    }
+
+    /**
+     * Determine locale, prefers Dutch
+     */
+    private getLocale(isDutch: boolean, isEnglish: boolean): string {
+        return isDutch ? 'nl' : isEnglish ? 'en' : '';
+    }
+}
