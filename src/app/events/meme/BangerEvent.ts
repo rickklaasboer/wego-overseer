a9770e8381d819be5ebc6efb4024890dd72c3878
Joël Kuijper <jmkuijper@outlook.com>
2024-03-04 19:28:48 +0100

    refactor: banger event to use service instead of private methods

--- src/app/events/meme/BangerEvent.ts
+++ src/app/events/meme/BangerEvent.ts
@@ -1,11 +1,11 @@
-import {trans} from '@/util/localization/localization';
 import BaseEvent from '@/app/events/BaseEvent';
+import ComprehendService from '@/app/services/aws/ComprehendService';
+import Logger from '@/telemetry/logger';
+import {Maybe} from '@/types/util';
+import {trans} from '@/util/localization/localization';
+import {LanguageCode} from '@aws-sdk/client-comprehend';
 import {Message} from 'discord.js';
 import {injectable} from 'tsyringe';
-import Logger from '@/telemetry/logger';
-import ComprehendService from '@/app/services/aws/ComprehendService';
-import {DetectDominantLanguageCommand} from '@aws-sdk/client-comprehend';
-import {Maybe} from '@/types/util';
 
 @injectable()
 export default class BangerEvent implements BaseEvent<'messageCreate'> {
@@ -25,10 +25,14 @@ export default class BangerEvent implements BaseEvent<'messageCreate'> {
             // Terminate if user is a bot
             if (message.author.bot) return;
 
-            const word = this.getLastWordEndingWithEr(message.content);
+            const word = this.getLastWordEndingWith(message.content, 'er');
             if (!word || word.length < 5) return;
 
-            const dominance = await this.getEnglishDominance(message.content);
+            const dominance = await this.comprehendService.getLanguageDominance(
+                message.content,
+                LanguageCode.EN,
+            );
+
             if (dominance < 0.75) {
                 this.logger.debug(
                     `Ignored non-English message: ${message.content}, because dominance is ${dominance} which is below the threshold of 0.75`,
@@ -45,27 +49,12 @@ export default class BangerEvent implements BaseEvent<'messageCreate'> {
     /**
      * Get the first word that ends with 'er'
      */
-    private getLastWordEndingWithEr(input: string): Maybe<string> {
+    private getLastWordEndingWith(input: string, word: string): Maybe<string> {
         // Split string into words
         const text = input
             .toLowerCase()
             .replace(/\b(\w)/g, (s) => s.toUpperCase());
         const words = text.split(' ');
-        return words.find((word) => word.endsWith('er'));
-    }
-
-    /**
-     * Get the dominance of English in input
-     */
-    private async getEnglishDominance(input: string): Promise<number> {
-        const client = this.comprehendService.getComprehendClient();
-        const {Languages} = await client.send(
-            new DetectDominantLanguageCommand({Text: input}),
-        );
-
-        return (
-            Languages?.find(({LanguageCode}) => LanguageCode === 'en')?.Score ??
-            0
-        );
+        return words.find((w) => w.endsWith(word));
     }
 }
