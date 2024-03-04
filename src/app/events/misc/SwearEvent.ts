fe1bb5ebd6d23d430e87d73250b57b4069919cfe
Joël Kuijper <jmkuijper@outlook.com>
2024-03-04 19:29:12 +0100

    feat(wip): add comprehend service

--- src/app/events/misc/SwearEvent.ts
+++ src/app/events/misc/SwearEvent.ts
@@ -1,4 +1,5 @@
 import BaseEvent from '@/app/events/BaseEvent';
+import ComprehendService from '@/app/services/aws/ComprehendService';
 import Logger from '@/telemetry/logger';
 import {Message} from 'discord.js';
 import {injectable} from 'tsyringe';
@@ -8,15 +9,21 @@ export default class SwearEvent implements BaseEvent<'messageCreate'> {
     public name = 'SwearEvent';
     public event = 'messageCreate' as const;
 
-    constructor(private logger: Logger) {}
+    constructor(
+        private comprehendService: ComprehendService,
+        private logger: Logger,
+    ) {}
 
     /**
      * Run the event
      */
     public async execute(message: Message<boolean>): Promise<void> {
         try {
-            // Terminate if user is a bot
             if (message.author.bot) return;
+
+            // TODO: Implement
+
+            // await message.reply('Please do not swear!');
         } catch (err) {
             this.logger.fatal('Failed to run SwearEvent', err);
         }
